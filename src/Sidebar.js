39fa01c9cb6d4d75693573e8ba5bedb1b46cdbe1
zarinzaidi <aereinzayed@yahoo.com>
2024-05-01 21:18:48 +0800

    Sidebar.js init

---
+++ src/Sidebar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Card } from 'react-bootstrap';
+
+function Sidebar() {
+    return (
+        <Card>
+            <Card.Body>
+                Sidebar Content
+            </Card.Body>
+        </Card>
+    );
+}
+
+export default Sidebar;
